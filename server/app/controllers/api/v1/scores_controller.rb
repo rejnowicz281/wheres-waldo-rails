d4eb8a591b2836a39de01601e056f91e5aaa37bf
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-07-09 16:23:15 +0200

    Fix scores controller

--- server/app/controllers/api/v1/scores_controller.rb
+++ server/app/controllers/api/v1/scores_controller.rb
@@ -1,24 +1,27 @@
 class Api::V1::ScoresController < ApplicationController
   # GET /scores
   def index
-    @scores = Score.all
+    @map = Map.find(params[:map_id])
+    @scores = @map.scores
 
     render json: @scores
   end
 
   # GET /scores/1
   def show
-    @score = Score.find(params[:id])
+    @map = Map.find(params[:map_id])
+    @score = @map.scores.find(params[:id])
 
     render json: @score
   end
 
   # POST /scores
   def create
-    @score = Score.new(score_params)
+    @map = Map.find(params[:map_id])
+    @score = @map.scores.build(score_params)
 
     if @score.save
-      render json: @score, status: :created, location: @score
+      render json: @score, status: :created, location: api_v1_map_score_url(@map, @score)
     else
       render json: @score.errors, status: :unprocessable_entity
     end
