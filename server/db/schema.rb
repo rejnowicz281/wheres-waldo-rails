d656e804b63344c865c0d32c46c275df47829568
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-07-09 16:23:44 +0200

    Add default value to score player_name

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_08_173101) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_09_142156) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_08_173101) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -56,7 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_08_173101) do
   end
 
   create_table "scores", force: :cascade do |t|
-    t.string "player_name"
+    t.string "player_name", default: "Anon"
     t.integer "seconds"
     t.integer "map_id", null: false
     t.datetime "created_at", null: false
