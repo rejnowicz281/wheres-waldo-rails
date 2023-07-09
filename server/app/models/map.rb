cdc69d2011f448e8266394fda4c1b75dc18ef3aa
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-07-09 12:10:55 +0200

    Order map's scores by seconds

--- server/app/models/map.rb
+++ server/app/models/map.rb
@@ -1,6 +1,6 @@
 class Map < ApplicationRecord
     has_many :characters
-    has_many :scores
+    has_many :scores, -> { order(seconds: :asc) }
     has_one_attached :image
 
     validates :name, presence: true
