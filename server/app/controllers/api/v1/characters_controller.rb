7b0c6616e3cdf217fadcb5436cd5026aa61367fa
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-07-09 15:32:48 +0200

    Fix character controller

--- server/app/controllers/api/v1/characters_controller.rb
+++ server/app/controllers/api/v1/characters_controller.rb
@@ -1,14 +1,16 @@
 class Api::V1::CharactersController < ApplicationController
   # GET /characters
   def index
-    @characters = Character.all
+    @map = Map.find(params[:map_id])
+    @characters = @map.characters
 
     render json: @characters
   end
 
   # GET /characters/1
   def show
-    @character = Character.find(params[:id])
+    @map = Map.find(params[:map_id])
+    @character = @map.characters.find(params[:id])
 
     render json: @character
   end
